fb7f1eba32cec71eeb59700452647fd758066bcc
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-19 23:41:23 -0700

    Update route.ts

--- app/api/qstash/route.ts
+++ app/api/qstash/route.ts
@@ -1,12 +1,18 @@
+import { NextResponse } from 'next/server';
 import { verifySignature } from "@upstash/qstash/nextjs";
-import { NextResponse } from "next/server";
 
-async function POST() {
-  console.log("If this is printed, the signature has already been verified");
+// This function will handle your POST request
+export async function POST(request, response) {
+  try {
+    console.log("If this is printed, the signature has already been verified");
 
-  NextResponse.json({'status': 200})
-  // do stuff
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    // Handle errors and send an appropriate response
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
 }
 
+// Use verifySignature to ensure the request is coming from a trusted source
 export default verifySignature(POST);
 
