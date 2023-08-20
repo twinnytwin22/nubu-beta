6164aee0015e6b398ecfb59d9438f522dd7c6124
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-19 23:24:41 -0700

    testing qstash

---
+++ app/api/qstash/route.ts
@@ -0,0 +1,17 @@
+import { verifySignature } from "@upstash/qstash/nextjs";
+import { NextResponse } from "next/server";
+
+async function POST() {
+  console.log("If this is printed, the signature has already been verified");
+
+  NextResponse.json({'status': 200})
+  // do stuff
+}
+
+export default verifySignature(POST);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
